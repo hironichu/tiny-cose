bc7753ec52d7eeae1c331c6fe26816246285f57e
Levi Schuck <me@levischuck.com>
2023-08-07 21:41:14 -0500

    Add specialized COSE Key interfaces

--- src/constants.ts
+++ src/constants.ts
@@ -3,21 +3,46 @@
 
 // COSE Algorithms
 export const ECDSA_SHA_256 = -7;
-export const EDDSA = -8;
 export const ECDSA_SHA_384 = -35;
 export const ECDSA_SHA_512 = -36;
+export type ECDSA_ALG =
+  | typeof ECDSA_SHA_256
+  | typeof ECDSA_SHA_384
+  | typeof ECDSA_SHA_512;
+
+// EdDSA
+export const EDDSA = -8;
 
 export const RSASSA_PSS_SHA_256 = -37;
 export const RSASSA_PSS_SHA_384 = -38;
 export const RSASSA_PSS_SHA_512 = -39;
+export type RSASSA_PSS_ALG =
+  | typeof RSASSA_PSS_SHA_256
+  | typeof RSASSA_PSS_SHA_384
+  | typeof RSASSA_PSS_SHA_512;
 
 export const RSASSA_PKCS1_v1_5_SHA_256 = -257;
 export const RSASSA_PKCS1_v1_5_SHA_384 = -256;
 export const RSASSA_PKCS1_v1_5_SHA_512 = -259;
+export type RSASSA_PKCS1_v1_5_ALG =
+  | typeof RSASSA_PKCS1_v1_5_SHA_256
+  | typeof RSASSA_PKCS1_v1_5_SHA_384
+  | typeof RSASSA_PKCS1_v1_5_SHA_512;
 
 export const HMAC_SHA_256 = 5;
 export const HMAC_SHA_384 = 6;
 export const HMAC_SHA_512 = 7;
+export type HMAC_SHA_ALG =
+  | typeof HMAC_SHA_256
+  | typeof HMAC_SHA_384
+  | typeof HMAC_SHA_512;
+
+export type ALG_ALL =
+  | ECDSA_ALG
+  | typeof EDDSA
+  | RSASSA_PSS_ALG
+  | RSASSA_PKCS1_v1_5_ALG
+  | HMAC_SHA_ALG;
 
 // COSE Key Common Parameters
 export const KTY = 1;
@@ -31,10 +56,10 @@ export const OKP_CURVE = -1;
 export const OKP_X = -2;
 export const OKP_D = -4;
 
-export const EC_CURVE = -1;
-export const EC_X = -2;
-export const EC_Y = -3;
-export const EC_D = -4;
+export const EC2_CURVE = -1;
+export const EC2_X = -2;
+export const EC2_Y = -3;
+export const EC2_D = -4;
 
 export const RSA_N = -1;
 export const RSA_E = -2;
@@ -49,16 +74,54 @@ export const OCTET_KEY = -1;
 
 // COSE Key Types
 export const KTY_OKP = 1;
-export const KTY_EC = 2;
+export const KTY_EC2 = 2;
 export const KTY_RSA = 3;
-export const KTY_OCTET = 4;
+export const KTY_SYMMETRIC = 4;
+export type KTY_ALL =
+  | typeof KTY_OKP
+  | typeof KTY_EC2
+  | typeof KTY_SYMMETRIC
+  | typeof KTY_RSA;
 
 // COSE Elliptic Curves
-export const EC_CURVE_P256 = 1;
-export const EC_CURVE_P384 = 2;
-export const EC_CURVE_P521 = 3;
+export const EC2_CRV_P256 = 1;
+export const EC2_CRV_P384 = 2;
+export const EC2_CRV_P521 = 3;
+export type EC2_CRV_ALL =
+  | typeof EC2_CRV_P256
+  | typeof EC2_CRV_P384
+  | typeof EC2_CRV_P521;
 
-export const OKP_CURVE_X25519 = 4;
-export const OKP_CURVE_X448 = 5;
-export const OKP_CURVE_ED25519 = 6;
-export const OKP_CURVE_ED448 = 7;
+// export const OKP_CRV_X25519 = 4;
+// export const OKP_CRV_X448 = 5;
+export const OKP_CRV_ED25519 = 6;
+export const OKP_CRV_ED448 = 7;
+// export type OKP_CRV_ECDH =
+//   | typeof OKP_CRV_X25519
+//   | typeof OKP_CRV_X448;
+export type OKP_CRV_EDDSA =
+  | typeof OKP_CRV_ED25519
+  | typeof OKP_CRV_ED448;
+
+export const KEY_OP_SIGN = 1;
+export const KEY_OP_VERIFY = 2;
+// export const KEY_OP_ENCRYPT = 3;
+// export const KEY_OP_DECRYPT = 4;
+// export const KEY_OP_WRAP_KEY = 5;
+// export const KEY_OP_UNWRAP_KEY = 6;
+// export const KEY_OP_DERIVE_KEY = 7;
+// export const KEY_OP_DERIVE_BITS = 8;
+export const KEY_OP_MAC_CREATE = 9;
+export const KEY_OP_MAC_VERIFY = 10;
+
+export type KEY_OPS_ALL =
+  | typeof KEY_OP_SIGN
+  | typeof KEY_OP_VERIFY
+  // | typeof KEY_OP_ENCRYPT
+  // | typeof KEY_OP_DECRYPT
+  // | typeof KEY_OP_WRAP_KEY
+  // | typeof KEY_OP_UNWRAP_KEY
+  // | typeof KEY_OP_DERIVE_KEY
+  // | typeof KEY_OP_DERIVE_BITS
+  | typeof KEY_OP_MAC_CREATE
+  | typeof KEY_OP_MAC_VERIFY;
---
+++ src/types.ts
@@ -0,0 +1,122 @@
+import {
+  ALG_ALL,
+  EC2_CRV_ALL,
+  ECDSA_ALG,
+  EDDSA,
+  HMAC_SHA_ALG,
+  KEY_OP_MAC_CREATE,
+  KEY_OP_MAC_VERIFY,
+  KEY_OP_SIGN,
+  KEY_OP_VERIFY,
+  KEY_OPS_ALL,
+  KTY_ALL,
+  KTY_EC2,
+  KTY_OKP,
+  KTY_RSA,
+  KTY_SYMMETRIC,
+  OKP_CRV_EDDSA,
+  RSASSA_PKCS1_v1_5_ALG,
+  RSASSA_PSS_ALG,
+} from "./constants.ts";
+
+export interface COSE_Key {
+  kty: KTY_ALL;
+  kid?: Uint8Array;
+  alg?: ALG_ALL;
+  key_ops?: KEY_OPS_ALL[];
+  // base_iv?: Uint8Array
+}
+
+export interface ECDSA_Public_COSE_Key extends COSE_Key {
+  kty: typeof KTY_EC2;
+  alg: ECDSA_ALG;
+  crv: EC2_CRV_ALL;
+  key_ops?: (typeof KEY_OP_VERIFY)[];
+  x: Uint8Array;
+  y: Uint8Array | true | false;
+}
+
+export interface ECDSA_Private_COSE_Key extends COSE_Key {
+  kty: typeof KTY_EC2;
+  alg: ECDSA_ALG;
+  crv: EC2_CRV_ALL;
+  key_ops?: (typeof KEY_OP_VERIFY | typeof KEY_OP_SIGN)[];
+  x?: Uint8Array;
+  y?: Uint8Array | true | false;
+  d: Uint8Array;
+}
+
+export interface EDDSA_Public_COSE_Key extends COSE_Key {
+  kty: typeof KTY_OKP;
+  alg: typeof EDDSA;
+  crv: OKP_CRV_EDDSA;
+  key_ops?: (typeof KEY_OP_VERIFY)[];
+  x: Uint8Array;
+}
+
+export interface EDDSA_Private_COSE_Key extends COSE_Key {
+  kty: typeof KTY_OKP;
+  alg: typeof EDDSA;
+  crv: OKP_CRV_EDDSA;
+  key_ops?: (typeof KEY_OP_VERIFY | typeof KEY_OP_SIGN)[];
+  x?: Uint8Array;
+  d: Uint8Array;
+}
+
+export interface RSA_Public_Key {
+  n: Uint8Array;
+  e: Uint8Array;
+}
+
+export interface RSA_Private_Key extends RSA_Public_Key {
+  p: Uint8Array;
+  q: Uint8Array;
+  dP: Uint8Array;
+  dQ: Uint8Array;
+  qInv: Uint8Array;
+  // Multi prime private keys are not supported.
+}
+
+export interface RSASSA_PKCS1_v1_5_Public_COSE_Key
+  extends COSE_Key, RSA_Public_Key {
+  kty: typeof KTY_RSA;
+  alg: RSASSA_PKCS1_v1_5_ALG;
+  key_ops?: (typeof KEY_OP_VERIFY)[];
+}
+
+export interface RSASSA_PKCS1_v1_5_Private_COSE_Key
+  extends COSE_Key, RSA_Private_Key {
+  kty: typeof KTY_RSA;
+  alg: RSASSA_PKCS1_v1_5_ALG;
+  key_ops?: (typeof KEY_OP_VERIFY | typeof KEY_OP_SIGN)[];
+}
+
+export interface RSASSA_PSS_Public_COSE_Key extends COSE_Key, RSA_Public_Key {
+  kty: typeof KTY_RSA;
+  alg: RSASSA_PSS_ALG;
+  key_ops?: (typeof KEY_OP_VERIFY)[];
+}
+
+export interface RSASSA_PSS_Private_COSE_Key extends COSE_Key, RSA_Private_Key {
+  kty: typeof KTY_RSA;
+  alg: RSASSA_PSS_ALG;
+  key_ops?: (typeof KEY_OP_VERIFY | typeof KEY_OP_SIGN)[];
+}
+
+export interface HMAC_COSE_Key extends COSE_Key {
+  kty: typeof KTY_SYMMETRIC;
+  alg: HMAC_SHA_ALG;
+  key_ops?: (typeof KEY_OP_MAC_CREATE | typeof KEY_OP_MAC_VERIFY)[];
+  k: Uint8Array;
+}
+
+export type COSE_Key_ALL =
+  | ECDSA_Public_COSE_Key
+  | ECDSA_Private_COSE_Key
+  | EDDSA_Public_COSE_Key
+  | EDDSA_Private_COSE_Key
+  | RSASSA_PKCS1_v1_5_Public_COSE_Key
+  | RSASSA_PKCS1_v1_5_Private_COSE_Key
+  | RSASSA_PSS_Public_COSE_Key
+  | RSASSA_PSS_Private_COSE_Key
+  | HMAC_COSE_Key;
