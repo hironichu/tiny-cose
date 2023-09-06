8cd5b3458b191c4b29ca25be5239a1adfef1a630
Levi Schuck <me@levischuck.com>
2023-09-06 13:43:29 -0500

    Format

--- src/deps.ts
+++ src/deps.ts
@@ -1,2 +1,5 @@
 export type { CBORType } from "https://deno.land/x/tiny_cbor@0.2.2/index.ts";
-export { decodeBase64Url, encodeBase64Url } from "https://deno.land/x/tiny_encodings@0.2.1/index.ts";
+export {
+  decodeBase64Url,
+  encodeBase64Url,
+} from "https://deno.land/x/tiny_encodings@0.2.1/index.ts";
