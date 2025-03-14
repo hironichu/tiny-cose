57f62203777b86e34d477fd608aa2c7e380395d3
Your Name <you@example.com>
2025-03-14 18:13:43 +0100

    Test

--- src/deps.ts
+++ src/deps.ts
@@ -1,5 +1,2 @@
-export type { CBORType } from "https://deno.land/x/tiny_cbor@0.2.2/index.ts";
-export {
-  decodeBase64Url,
-  encodeBase64Url,
-} from "https://deno.land/x/tiny_encodings@0.2.1/index.ts";
+export type { CBORType } from "@levischuck/tiny-cbor";
+export { decodeBase64Url, encodeBase64Url } from "@std/encoding";
